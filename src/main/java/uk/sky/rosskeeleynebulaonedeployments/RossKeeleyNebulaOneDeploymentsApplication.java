38a7313cbcedfd43a8a0e648315f5593c4a29d05
Ross Keeley <ross.keeley@sky.uk>
2023-03-21 14:29:55 +0000

    adding initial spring boot starter template

---
+++ src/main/java/uk/sky/rosskeeleynebulaonedeployments/RossKeeleyNebulaOneDeploymentsApplication.java
@@ -0,0 +1,13 @@
+package uk.sky.rosskeeleynebulaonedeployments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RossKeeleyNebulaOneDeploymentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RossKeeleyNebulaOneDeploymentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uk/sky/rosskeeleynebulaonedeployments/RossKeeleyNebulaOneDeploymentsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.sky.rosskeeleynebulaonedeployments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RossKeeleyNebulaOneDeploymentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
